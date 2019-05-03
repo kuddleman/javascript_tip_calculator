649718f80f483a3416ac3c8931bb1d42e9eee7dc
Donny <EDL1155@gmail.com>
2019-05-02 21:27:10 -0700

    added constants to js file

---
+++ tip.js
@@ -0,0 +1,5 @@
+const range = document.getElementById('tipRange');
+const tipOut = document.getElementById('percentOut');
+const btnCalculate = document.getElementById('btnCalculate');
+const billAmount = document.getElementById('billAmount');
+const resultArea = document.getElementById('result');
